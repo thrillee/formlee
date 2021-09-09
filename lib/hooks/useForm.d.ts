650d6318fe3cfc1494c983af0a0899e48b057a2d
Oluwatobi <bellotobiloba01@gmail.com>
2021-09-09 13:48:55 +0100

    Build

--- lib/hooks/useForm.d.ts
+++ lib/hooks/useForm.d.ts
@@ -2,6 +2,5 @@ import * as React from 'react';
 import { Value } from '../types/formConfigs';
 export declare const useForm: (data: Value) => {
     values: Value;
-    updateValues: (value: Value) => void;
     handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
